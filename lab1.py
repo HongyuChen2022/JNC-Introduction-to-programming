3f4113ab5685b7070e2114360c0aafbafca2384a
HongyuChen2022 <105467896+HongyuChen2022@users.noreply.github.com>
2022-05-13 02:55:17 +0200

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,121 @@
+
+print("Hello World")
+print("Hello everyone", end = ",")
+greet = "good morning"
+print(greet)
+
+print("Hello everyone")
+greet = "good morning"
+
+print(greet)
+
+####### |Type of Data| #######
+
+# string
+
+#string_1 = "this is a string"
+#string_2 = "12345"
+#string_3 = "3.1415926"
+
+#print(string_1)
+#print(string_2)
+#print(type(string_1))
+
+
+# integer
+
+#integer_1 = 1009248
+#print(type(integer_1))
+
+
+# float
+
+#float_1 = 3.141526
+#print(type(float_1))
+
+
+# data type conversion
+# string to integer
+
+#int(string_2)
+
+#print(type(int(string_2)))
+
+# integer to string
+#str(integer_1)
+
+
+# string to float
+
+#float(string_3)
+
+
+# float to integer
+#int(float_1)
+
+#----------------------------------------------------------------
+
+
+
+
+## Backslash
+
+#print("The path is c: \\tem\\data\\numbers")
+
+#print('I\'d like to watch\"Iron Man 1\" tonight')
+
+#-----------------------------------------------------------------
+
+## format()
+
+#"{:2d}".format(100)
+#"{:.2f}".format(100)
+#"{:.2e}".format(100)
+
+
+
+#------------------------------------------------------------------
+
+
+####### |Variable| #######
+
+#firstname = "Sherlock"
+#lastname = "Holmes"
+#height = 1.80
+#age = 48
+#weigh = 48
+#price = 123 #$
+
+#print(firstname, "\'s family name is", lastname)
+#print(firstname + lastname, "\'s height is", height )
+
+#print("{} {}\'s hegiht is {}".format(firstname, lastname, height))
+
+
+#------------------------------
+# operation
+
+#a = 1
+#b = 2
+#c = 3
+
+#print(b**c)
+#print(c//b)
+#print(c%b)
+
+
+
+#name_asking = input("what is your name?")
+#print("hi",name_asking)
+
+# currency rate exchange example
+
+
+
+#rmb  = float(input("how much RMB do you want to exchange for USD?"))
+
+#usd = rmb/6.7
+
+#print("Here is your money: ", format(usd, '.2f'))
+
+
